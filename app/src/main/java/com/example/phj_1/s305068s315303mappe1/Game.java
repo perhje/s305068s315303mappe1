ea16da070bc6a94cd5c8665662d74fe94cba1516
perhje <38944777+perhje@users.noreply.github.com>
2018-09-10 12:18:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
@@ -0,0 +1,7 @@
+package com.example.phj_1.s305068s315303mappe1;
+
+import android.app.Activity;
+
+public class Game extends Activity{
+
+}
---
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.phj_1.s305068s315303mappe1;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
