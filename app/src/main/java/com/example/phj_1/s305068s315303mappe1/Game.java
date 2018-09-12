fe87a9f0d8ebd7a5bd57b6408eeefa6eed81c0dc
perhje <38944777+perhje@users.noreply.github.com>
2018-09-12 12:42:41 +0200

    gvkki

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
@@ -1,7 +1,12 @@
 package com.example.phj_1.s305068s315303mappe1;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Game extends Activity{
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
 }
