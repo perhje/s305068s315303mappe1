1d1835b713dd85aa6d3b1587ef699958f313d059
perhje <38944777+perhje@users.noreply.github.com>
2018-09-12 11:35:32 +0200

    gvkki

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/MainActivity.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.phj_1.s305068s315303mappe1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.game:
+                Intent i=new Intent(this,Game.class);
+                startActivity(i);
+                break;
+        }
+        return true;
     }
 }
