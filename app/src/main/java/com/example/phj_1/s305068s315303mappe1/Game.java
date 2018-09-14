a42ffc40b32bde3901bb5241a8c15d59ea51a762
perhje <38944777+perhje@users.noreply.github.com>
2018-09-14 15:56:39 +0200

    grid

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
@@ -1,10 +1,14 @@
 package com.example.phj_1.s305068s315303mappe1;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Game extends Activity{
+    public String input = "0";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,8 +16,10 @@ public class Game extends Activity{
     }
 
     public void showOne(){
-        
+        input = "1";
     }
+
+    public String inputString  = getString(R.string.input, input);
 
     public void nextQuestion(){
 
