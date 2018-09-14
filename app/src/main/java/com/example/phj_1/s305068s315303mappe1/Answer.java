ece7a3be148d9718a6bcf27265b17e198bbe7972
perhje <38944777+perhje@users.noreply.github.com>
2018-09-14 13:29:46 +0200

    grid

---
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Answer.java
@@ -0,0 +1,6 @@
+package com.example.phj_1.s305068s315303mappe1;
+
+import android.app.Fragment;
+
+public class Answer extends Fragment {
+}
--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
@@ -9,4 +9,6 @@ public class Game extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+
 }
