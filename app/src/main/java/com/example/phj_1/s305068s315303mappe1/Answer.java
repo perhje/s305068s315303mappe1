89d6a011522ca95d9f4cd66b40ecb787382ac5fb
perhje <38944777+perhje@users.noreply.github.com>
2018-09-14 14:09:20 +0200

    grid

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Answer.java
+++
@@ -1,6 +0,0 @@
-package com.example.phj_1.s305068s315303mappe1;
-
-import android.app.Fragment;
-
-public class Answer extends Fragment {
-}
--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
@@ -2,12 +2,21 @@ package com.example.phj_1.s305068s315303mappe1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Game extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void showOne(){
+        
+    }
+
+    public void nextQuestion(){
+
     }
 
 
