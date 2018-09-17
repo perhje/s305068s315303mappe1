9832698ebdf57e5320ca3c1c0abc884d7a6e015a
perhje <38944777+perhje@users.noreply.github.com>
2018-09-17 11:16:24 +0200

    grid

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Preferences.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Preferences.java
@@ -1,4 +1,12 @@
 package com.example.phj_1.s305068s315303mappe1;
 
-public class Preferences {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Preferences extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
 }
--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Statistics.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Statistics.java
@@ -1,4 +1,12 @@
 package com.example.phj_1.s305068s315303mappe1;
 
-public class Statistics {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Statistics extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
 }
