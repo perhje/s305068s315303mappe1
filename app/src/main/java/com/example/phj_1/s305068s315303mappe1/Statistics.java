d492142bae0506544983957966e7b0be67984686
perhje <38944777+perhje@users.noreply.github.com>
2018-09-17 13:39:42 +0200

    grid

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Statistics.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Statistics.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Statistics extends Activity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
