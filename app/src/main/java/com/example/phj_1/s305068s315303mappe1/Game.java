a6d30117431cd37ac23a1b744ba9b309f6a1aad5
Nguyen <42539924+s315303@users.noreply.github.com>
2018-09-18 15:45:05 +0200

    hei.. legger ut koden min..slik vi kan legge det inn.

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
@@ -3,12 +3,26 @@ package com.example.phj_1.s305068s315303mappe1;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Game extends Activity{
 
     public String input = "0";
+    Button knapp1;
+    Button knapp2;
+    Button knapp3;
+    Button knapp4;
+    Button knapp5;
+    Button knapp6;
+    Button knapp7;
+    Button knapp8;
+    Button knapp9;
+    Button knapp0;
+    Button submit;
+    Button avbryt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
