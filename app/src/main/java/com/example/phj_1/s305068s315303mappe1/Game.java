3bf556a8fab0db5b067678fabcc408f6edb3f869
perhje <38944777+perhje@users.noreply.github.com>
2018-09-20 22:37:55 +0200

    ssed

--- app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe1/Game.java
@@ -2,6 +2,7 @@ package com.example.phj_1.s305068s315303mappe1;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -231,7 +232,9 @@ public class Game extends Activity {
 
 
     private void finishquiz(){
-
+        //Statistics.feil = Statistics.feil + feil;
+        //Statistics.riktig = Statistics.riktig + riktig;
+        finish();
     }
 }
 
